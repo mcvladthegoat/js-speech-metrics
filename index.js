80e07e6a07de37e449ddad49e9c7b53e3db815b4
mcvladthegoat <swimer96@mail.ru>
2019-02-07 23:16:41 +0300

    small fix

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-function calculate(data) {
+const calculate = (data) => { 
         if (Object.prototype.toString.call(data) !== '[object Array]') throw new TypeError("Speech-metrics module needs an array!");
+        const removePunctuation = (str) => str.replace(/(~|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g, "");
 
-        function removePunctuation(str) {
-                return str.replace(/(~|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g,"");
-        }
-
-        var result = [],
+        let result = [],
             SER = 0,
-            WCRavg = 0,
+            WCRavg = 0, 
             WERavg = 0;
 
-        data.forEach(function(item, index, arr) {
+        data.forEach((item, index, arr) => {
             if (item.length !== 2) {
                 throw new TypeError("All array elements should be an array with length 2.");
             }
-            var r = removePunctuation(item[0].toLowerCase().trim()).split(' '),
+            let r = removePunctuation(item[0].toLowerCase().trim()).split(' '),
                 h = removePunctuation(item[1].toLowerCase().trim()).split(' ');
 
-            var deletion=0, substitution=0, insertion=0, correct=0;
-            var d = [...Array(r.length + 1)].map(function(x) {
+            let deletion = 0, substitution = 0, insertion = 0, correct = 0;
+            let d = [...Array(r.length + 1)].map((x) => {
                 return Array(h.length + 1).fill(0);
             });
 
-            for(var i = 0; i < r.length; i++) {
-                for(var j = 0; j < h.length; j++) {
+            for(let i = 0; i < r.length; i++) {
+                for(let j = 0; j < h.length; j++) {
                     if (i == 0) {
                         d[0][j] = j;
                     } else if (j == 0) {
@@ -32,8 +29,8 @@ function calculate(data) {
                 }
             }
           
-            for(i = 1; i < r.length + 1; i++) {
-                for(j = 1; j < h.length + 1; j++) {
+            for(let i = 1; i < r.length + 1; i++) {
+                for(let j = 1; j < h.length + 1; j++) {
                     if (r[i-1] == h[j-1]) {
                         d[i][j] = d[i-1][j-1];
                     } else {
@@ -45,7 +42,7 @@ function calculate(data) {
                 }
             }
 
-            var WER = d[r.length][h.length] / r.length,
+            let WER = d[r.length][h.length] / r.length,
                 WCR = 0,
                 x = r.length,
                 y = h.length;
